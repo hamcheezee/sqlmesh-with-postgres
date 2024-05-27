edb30a4e73e0b3bf1af115652687b04eae9d2653
cheeze <nongnaphat.wongpiyachai@gmail.com>
2024-05-27 23:24:45 +0700

    Disable Airflow example DAGs

--- src/airflow/docker_compose_decorator.py
+++ src/airflow/docker_compose_decorator.py
@@ -7,13 +7,24 @@ DOCKER_COMPOSE_YAML = "docker-compose.yaml"
 yaml = YAML(typ="safe")
 yaml.default_flow_style = False
 
+# Load the Docker Compose file
 with open(DOCKER_COMPOSE_YAML, "r", encoding="utf-8") as fd:
     docker_compose = yaml.load(fd)
 
-# Don't load Airflow example DAGs
-docker_compose["x-airflow-common"]["environment"]["AIRFLOW_CORE_LOAD_EXAMPLES"] = "false"
+# Update environment variables to disable loading Airflow example DAGs
+if "x-airflow-common" in docker_compose:
+    docker_compose["x-airflow-common"].setdefault("environment", {})
+    docker_compose["x-airflow-common"]["environment"]["AIRFLOW_CORE_LOAD_EXAMPLES"] = "false"
 
-docker_compose["services"]["postgres"]["ports"] = ["5432:5432"]
+for service in ["airflow-cli", "airflow-init", "airflow-scheduler", "airflow-worker"]:
+    if "services" in docker_compose and service in docker_compose["services"]:
+        docker_compose["services"][service].setdefault("environment", {})
+        docker_compose["services"][service]["environment"]["AIRFLOW__CORE__LOAD_EXAMPLES"] = "false"
 
+# Set Postgres port mapping
+if "services" in docker_compose and "postgres" in docker_compose["services"]:
+    docker_compose["services"]["postgres"]["ports"] = ["5432:5432"]
+
+# Save the updated Docker Compose file
 with open(DOCKER_COMPOSE_YAML, "w", encoding="utf-8") as fd:
     yaml.dump(docker_compose, fd)
